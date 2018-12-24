4b90abce2f12d3c981152d6bc854857758f90440
Sergey11111 <sergey45684745@gmail.com>
2018-12-24 12:54:21 +0300

    refactoring...

--- src/main/java/ru/job4j/springapp/model/Role.java
+++ src/main/java/ru/job4j/springapp/model/Role.java
@@ -1,5 +1,8 @@
 package ru.job4j.springapp.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -9,6 +12,8 @@ import java.util.Set;
 
 @Entity
 @Table(name = "roles")
+@Data
+@NoArgsConstructor
 public class Role {
 
     @Id
@@ -21,39 +26,4 @@ public class Role {
     @ManyToMany(mappedBy = "roles")
     private Set<ru.job4j.springapp.model.User> users;
 
-    public Role() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<ru.job4j.springapp.model.User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(Set<ru.job4j.springapp.model.User> users) {
-        this.users = users;
-    }
-
-    @Override
-    public String toString() {
-        return "Role{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", users=" + users +
-                '}';
-    }
 }
--- src/main/java/ru/job4j/springapp/model/User.java
+++ src/main/java/ru/job4j/springapp/model/User.java
@@ -1,5 +1,8 @@
 package ru.job4j.springapp.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -8,6 +11,8 @@ import java.util.Set;
  */
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -28,44 +33,5 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
 
